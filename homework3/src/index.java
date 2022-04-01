bcf98f09987f93d58454455202bdafe5581622ee
atuan1605 <anhtuan097856@gmail.com>
2022-04-01 16:13:18 +0700

    update

--- homework3/src/index.java
+++ homework3/src/index.java
@@ -12,15 +12,12 @@ public class index {
         for (int i = 0; i < str.length(); i++) {
             if (ch == str.charAt(i)) {
                 count++;
+                System.out.println("Kí hiệu " + ch + " cần tìm có index là: " + i);
             }
         }
         System.out.println("Số lần xuất hiện của " + ch + " là " + count);
 
-        int e1 = str.indexOf('e');
-        int e2 = str.indexOf('e',5);
-        int e3 = str.indexOf('e',7);
-        int e4 = str.indexOf('e',10);
-        System.out.println("e có index là: "+ e1 +" " +e2 + " " +e3 + " " +e4);
+
 
     }
 }
